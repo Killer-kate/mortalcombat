99c75615614c68b981fae33a31972a20ae85a052
Killer-kate <75667457+Killer-kate@users.noreply.github.com>
2022-02-17 19:37:08 +0300

    create homework3

--- main.js
+++ main.js
@@ -1,52 +1,95 @@
+const $arenas = document.querySelector('.arenas');
+const $randomButton = document.querySelector('.button');
+
+
+
 // TASK 0
-let player1 = {
-    name: "XIAO",
+const player1 = {
+    player: 1,
+    name: 'XIAO',
     hp: 100,
-    img: "http://reactmarathon-api.herokuapp.com/assets/subzero.gif",
+    img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: ["Двуручный", "Одноручный", "Лук", "Копьё", "Книга"],
-    atack: (function () {
-        console.log(player1.name + " Fight...");
-    })
+    atack: function(name) {
+        console.log(name + ' Fight...');
+    },
 };
-let player2 = {
-    name: "MIKO",
-    hp: 80,
-    img: "http://reactmarathon-api.herokuapp.com/assets/sonya.gif",
+const player2 = {
+    player: 2,
+    name: 'MIKO',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
     weapon: ["Двуручный", "Одноручный", "Лук", "Копьё", "Книга"],
-    atack: (function () {
-        console.log(player2.name + " Fight...");
-    })
+    atack: function(name) {
+        console.log(name + " Fight...");
+    },
 };
 
-// TASK 1
-function createPlayer(classList, charName, life, img) {
-    // Создание элементов, поик класса
-    const $player = document.createElement('div');
-    const $progressbar = document.createElement('div');
-    const $character = document.createElement('div');
-    const $life = document.createElement('div');
-    const $name = document.createElement('div');
-    const $img = document.createElement('img');
-    const $arenas = document.querySelector('.arenas');
-    // Добавление классов для блоков
-    $player.classList.add(classList);
-    $progressbar.classList.add('progressbar');
-    $character.classList.add('character');
-    $life.classList.add('life');
-    $name.classList.add('name');
-    $img.src = img;
-
-    $life.style.width = life + '%';
-    $name.innerText =  charName;
-
-    $arenas.appendChild($player);
-    $player.appendChild($progressbar);
-    $player.appendChild($character);
-    $progressbar.appendChild($life);
-    $progressbar.appendChild($name);
-    $character.appendChild($img);
+function getRandom(random){
+    return Math.ceil(Math.random() * random);
 }
 
+function createElement(tag, className){
+    const $tag = document.createElement(tag);
+    if(className){
+          $tag.classList.add(className);  
+    }
+
+    return $tag;
+}
+
+
+function createPlayer(playerObj) {
+    // Создание элементов, поик класса
+    const $player = createElement('div', 'player'+playerObj.player);
+    const $progressbar = createElement('div', 'progressbar');
+    const $character = createElement('div', 'character');
+    const $life = createElement('div', 'life');
+    const $name = createElement('div', 'name');
+    const $img = createElement('img');
+
+    $life.style.width = playerObj.hp + '%';
+    $name.innerText =  playerObj.name;
+    $img.src = playerObj.img;
+    
+    $progressbar.appendChild($name);
+    $progressbar.appendChild($life);
+    
+    $character.appendChild($img);
+
+    $player.appendChild($progressbar);
+    $player.appendChild($character);
+    
+    return $player;
+}
+    
+
+function changeHP(player){
+    const $playerLife = document.querySelector('.player'+player.player+' .life');
+    if(player.hp > 0){
+        player.hp -= getRandom(20);
+    $playerLife.style.width = player.hp + '%';
+    } else if(player.hp <= 0){
+        player.hp = 0;
+        $arenas.appendChild(playerLose(player.name));
+        $randomButton.disabled = true;
+    }
+    console.log(player.hp);
+}
+
+function playerLose(name){
+    const $loseTitle = createElement('div', 'loseTitle');
+    $loseTitle.innerText = name + '  lose';
+    return $loseTitle;
+}
+
+$randomButton.addEventListener('click', function(){
+    console.log('####: Click Random Button');
+    changeHP(player1);
+    changeHP(player2);
+    
+})
+
+$arenas.appendChild(createPlayer(player1));
+$arenas.appendChild(createPlayer(player2));
 // TASK 2
-createPlayer('player1', player1.name, 50, player1.img);
-createPlayer('player2', player2.name, 80, player2.img);
